2256f0ab8164e88537eed9b8aee9f846140a4645
Jismol <jismol2007@gmail.com>
2021-07-04 22:11:24 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,24 @@
+// step 1: create function displaying numbers.
+// function displayNumber(num){
+// inpt.value+=num
+// }
+let displayNumber=(num)=>inpt.value+=num;
+// function evaluateExpression()
+// {
+//     let res=eval(inpt.value);
+//     inpt.value=res;
+// }
+let evaluateExpression=()=>inpt.value=eval(inpt.value)
+
+// function clearBox()
+// {
+//     inpt.value="";
+// }
+let clearBox=()=>inpt.value=""
+
+// function removeNumber()
+// {
+//     inpt.value=inpt.value.slice(0,-1)
+// }
+
+let removeNumber=()=>inpt.value=inpt.value.slice(0,-1)
